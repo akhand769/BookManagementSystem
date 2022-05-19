79be561411195beb18270f3d10fe2f21b21088d0
akhand chaurasia <akhand769@gmail.com>
2022-05-19 10:51:26 +0530

    First

--- BookManagementSystem/Form1.Designer.cs
+++ BookManagementSystem/Form1.Designer.cs
@@ -36,7 +36,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(347, 46);
+            this.label1.Location = new System.Drawing.Point(285, 39);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(186, 20);
             this.label1.TabIndex = 0;
