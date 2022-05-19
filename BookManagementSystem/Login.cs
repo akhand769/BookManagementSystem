749093a4d4a18a279ec001ac03ca31bcb4a4ec77
akhand chaurasia <akhand769@gmail.com>
2022-05-19 11:39:17 +0530

    1st page

--- BookManagementSystem/Login.cs
+++ BookManagementSystem/Login.cs
@@ -16,5 +16,9 @@ namespace BookManagementSystem
         {
             InitializeComponent();
         }
+        private void label8_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
     }
 }
